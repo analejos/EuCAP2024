63c2931ef29bbaeda1980f040eb709d9a1fd17be
analejos <162150771+analejos@users.noreply.github.com>
2024-03-21 11:16:45 +0100

    Add files via upload

---
+++ PartII/calc_error.m
@@ -0,0 +1,15 @@
+
+n = 8; %- bits
+resolution = 360/(2^n)
+N = 4;
+
+alpha = 10;
+k = (0:1:N-1)
+ph_th = 180.*k.*sin(alpha*pi/180)
+ph_bfic = ph_th./resolution
+ph_code = round(ph_bfic)
+
+ph_p = ph_code.*resolution;
+error_ph = ph_th - ph_p
+
+(20*pi/180)
